8095b543173decf59112044ff53d434952c30843
AlfredYChung <chung.alfred@gmail.com>
2014-10-16 22:38:31 -0700

    mid sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp2::Application.routes.draw do
-  get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  resources :users
   root "static_pages#home"
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
